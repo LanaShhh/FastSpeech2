eddc0ef86586585d793cc2f095b1457b624f45da
LanaShhh <lana.shhh@yandex.ru>
2023-11-23 15:54:59 +0300

    new inference

---
+++ inference.py
@@ -0,0 +1,30 @@
+import argparse
+
+import torch
+from tqdm import tqdm
+
+import utils
+from configs import mel_config, train_config, model_config
+from model.FastSpeech2 import FastSpeech2
+from synthesis import make_audio
+
+parser = argparse.ArgumentParser(prog="FastSpeech2 inference")
+
+parser.add_argument('model_state_dict_path', type=str,
+                    help='relative path for model state dict')
+
+args = parser.parse_args()
+
+WaveGlow = utils.get_WaveGlow()
+WaveGlow = WaveGlow.cuda()
+
+model = FastSpeech2(mel_config, model_config)
+model.load_state_dict(torch.load(args.model_state_dict_path, map_location='cuda:0')['model'])
+model = model.eval()
+
+texts = train_config.texts
+
+for config in train_config.configs:
+    for i, text in tqdm(enumerate(texts)):
+        make_audio(model, WaveGlow, text=text, path=train_config.inf_audio_path, res_prefix="res",
+                   speed_coef=config["speed_coef"], pitch_coef=config["pitch_coef"], energy_coef=config["energy_coef"])
